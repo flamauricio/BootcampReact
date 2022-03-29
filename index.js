08eb5ab040d0160468cbddb78b3031bc00cffa6f
Flavio Mauricio <flavio.m.valerio@accenture.com>
2022-03-29 09:38:54 -0300

    remontando estruturas

---
+++ index.js
@@ -0,0 +1,42 @@
+const container = document.getElementById("app");
+
+function Votacao(props) {
+
+    // let contador = 0;
+    const [contador, setContador] = React.useState(0);
+
+    function votarMais() {
+        setContador(contador + 1);
+    }
+
+    function votarMenos() {
+        setContador(contador - 1);
+    }
+
+    function votarAcresentar() {
+        setContador(contador + 10);
+    }
+
+    return(
+        <div>
+            <h3>{props.titulo}</h3>
+            Votos: {contador} <br />
+            <button onClick={votarMenos}>-</button>
+            <button onClick={votarMais}>+</button>
+            <button onClick={votarAcresentar}>Acresentar 10</button>
+        </div>
+    );
+}
+
+function App () {
+    return (
+        <div> 
+            <Votacao titulo="Cavaleiro das Trevas"/>
+            <Votacao titulo="The Batman"/>
+            <Votacao titulo="Batman Begins"/>
+        </div>
+    );
+}
+
+
+ReactDOM.render(App(), container);
